51c43a4d0ca90060ab8138cc81aa36d7797d4b84
Michal Svačina <msvacina@outlook.com>
2025-11-02 19:19:36 +0100

    Updated base vim config

--- lua/snack/config.lua
+++ lua/snack/config.lua
@@ -23,3 +23,21 @@ vim.cmd('filetype indent on')
 vim.o.wildmenu = true
 vim.o.wildmode = 'list:longest'
 vim.o.wildignore = '*.docx,*.jpg,*.png,*.gif,*.pdf,*.pyc,*.exe,*.flv,*.img,*.xlsx'
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
+
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+vim.opt.isfname:append("@-@")
+
+vim.opt.updatetime = 50
+
+vim.opt.colorcolumn = "80"
