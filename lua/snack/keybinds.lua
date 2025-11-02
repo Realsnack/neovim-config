71aa42e27d2552f189e0000c8ffda7726051b93b
Michal Svačina <msvacina@outlook.com>
2025-11-02 13:50:20 +0100

    Added telescope

--- lua/snack/keybinds.lua
+++ lua/snack/keybinds.lua
@@ -1,5 +1,5 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
@@ -8,4 +8,9 @@ vim.keymap.set("i", "<C-c>", "<Esc>")
 
 vim.keymap.set("n", "<leader>qq", ":q<CR>")
 vim.keymap.set("n", "<leader>wq", ":wq<CR>")
+
+-- Plugin keybinds
+
+-- Nvim tree
+vim.keymap.set("n", "<leader><leader>", vim.cmd.NvimTreeToggle)
 
